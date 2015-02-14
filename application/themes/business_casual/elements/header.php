1941de045540f977437c3ebe6c3a57b71c87f9a7
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2015-02-14 21:46:10 +0900

    Move google fonts to the preset

--- application/themes/business_casual/elements/header.php
+++ application/themes/business_casual/elements/header.php
@@ -14,10 +14,6 @@
     <!-- Custom CSS -->
     <link href="<?php echo $view->getStylesheet('business-casual.less'); ?>" rel="stylesheet">
 
-    <!-- Fonts -->
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
-
 </head>
 
 <body>
