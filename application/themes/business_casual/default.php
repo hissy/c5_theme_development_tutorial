3b3130f6077a4a624659bf42d8946db4f7b58f4e
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2015-02-14 15:10:01 +0900

    CSS Tweaks

--- application/themes/business_casual/default.php
+++ application/themes/business_casual/default.php
@@ -2,10 +2,12 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 $this->inc('elements/header.php'); ?>
 
+<main>
     <?php
     $a = new Area('Main');
     $a->enableGridContainer();
     $a->display($c);
     ?>
+</main>
 
 <?php  $this->inc('elements/footer.php'); ?>
