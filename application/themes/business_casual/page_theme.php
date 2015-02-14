b4e015110236d7466679e5c41458d6612a0b3ec1
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2015-02-14 15:14:02 +0900

    Requiring Core assets

--- application/themes/business_casual/page_theme.php
+++ application/themes/business_casual/page_theme.php
@@ -4,4 +4,10 @@ namespace Application\Theme\BusinessCasual;
 class PageTheme extends \Concrete\Core\Page\Theme\Theme
 {
     protected $pThemeGridFrameworkHandle = 'bootstrap3';
+
+    public function registerAssets() {
+        $this->providesAsset('javascript', 'bootstrap/*');
+        $this->providesAsset('css', 'bootstrap/*');
+        $this->requireAsset('javascript', 'jquery');
+    }
 }
