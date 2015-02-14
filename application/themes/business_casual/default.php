611b324deb49fc990159ab7d0f4e03f248295fe4
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2015-02-14 12:40:15 +0900

    Add theme path to assets

--- application/themes/business_casual/default.php
+++ application/themes/business_casual/default.php
@@ -9,10 +9,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo $this->getThemePath()?>/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="css/business-casual.css" rel="stylesheet">
+    <link href="<?php echo $this->getThemePath()?>/css/business-casual.css" rel="stylesheet">
 
     <!-- Fonts -->
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
@@ -168,7 +168,7 @@
     <?php Loader::element('footer_required'); ?>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="<?php echo $this->getThemePath()?>/js/bootstrap.min.js"></script>
 
     <!-- Script to Activate the Carousel -->
     <script>
